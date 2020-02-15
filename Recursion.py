7d291d0bba5d5fc828a09e8e4467bd47d505b94e
wonheejo <wonheejo@gmail.com>
2020-02-16 03:04:49 +0900

    Recursion page added and two problems solved

---
+++ Recursion.py
@@ -0,0 +1,28 @@
+
+# February 16th 2020
+# Problem 10872: Factorial
+"""
+x = int(input())
+
+def recursion(x):
+    if x == 1 or x == 0:
+        return 1
+    else:
+        return x*recursion(x-1)
+
+print(recursion(x))
+
+
+# Problem 10870: Fibonacci
+x = int(input())
+
+def fibonacci(x):
+    if x == 0 :
+        return 0
+    elif x <=2 :
+        return 1
+    else:
+        return fibonacci(x-2)+fibonacci(x-1)
+
+print(fibonacci(x))
+"""
